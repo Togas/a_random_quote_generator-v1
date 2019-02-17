eda323c9a8f6c0a43c5bba8894c71183702ca18d
Ben Stucke <ben-stucke@hotmail.de>
2019-02-17 19:26:55 +0100

    added quote

--- js/script.js
+++ js/script.js
@@ -9,17 +9,26 @@ FSJS project 1 - A Random Quote Generator
 /*** 
 declare and initialize quotes and put it into an array 
 ***/
-var quote1={ quote:"You have to do the best with what God gave you.", source:"Mrs. Gump", year:1994, citation:"Forrest Gump Movie"}
-var quote2={ quote:"Adapt what is useful, reject what is useless, and add what is specifically your own.", source:"Bruce Lee"}
-var quote3={ quote:"You can do anything, but not everything", source:"David Allen"}
+var quote1={ quote:"You have to do the best with what God gave you.", 
+source:"Mrs. Gump", year:1994, citation:"Forrest Gump Movie"}
+var quote2={ quote:"Adapt what is useful, reject what is useless, and add what is specifically your own.",
+ source:"Bruce Lee"}
+var quote3={ quote:"You can do anything, but not everything", 
+source:"David Allen"}
 var quote4={ quote:"One does not discover new lands without consenting to lose sight of the shore for a very long time.",
 source:"André Gide"}
-var quote5={ quote:"Deadlines are lifelines.", source:"Beth Lapides"}
-var quote6={ quote:"Don't cry because it's over, smile because it happened.", source:"Dr. Seuss"}
-var quote7={ quote:"Be yourself; everyone else is already taken.", source:"Oscar Wilde"}
-var quote8={ quote:"You shouldn’t focus on why you can’t do something, which is what most people do. You should focus on why perhaps, you could be one of the exceptions", source:"Steve Case"}
+var quote5={ quote:"Deadlines are lifelines.", 
+source:"Beth Lapides"}
+var quote6={ quote:"Don't cry because it's over, smile because it happened.", 
+source:"Dr. Seuss"}
+var quote7={ quote:"Be yourself; everyone else is already taken.", 
+source:"Oscar Wilde"}
+var quote8={ quote:"You shouldn’t focus on why you can’t do something, which is what most people do. You should focus on why perhaps, you could be one of the exceptions", 
+source:"Steve Case"}
+var quote9= {quote:"The art of debugging is figuring out what you really told your program to do rather than what you thought you told it to do.", 
+source:"Andrew Singer"}
 
-var quotes=[quote1, quote2, quote3, quote4, quote5, quote6, quote7, quote8];
+var quotes=[quote1, quote2, quote3, quote4, quote5, quote6, quote7, quote8, quote9];
 
 
 
